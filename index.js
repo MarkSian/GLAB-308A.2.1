41d436ae3b042a06555d518d5c6d8f227353f14f
MarkSian <merksianipar@gmail.com>
2025-04-03 15:15:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -38,3 +38,32 @@ for (let i = 0; i < adventurer.inventory.length; i++) {
     console.log(adventurer.inventory[i]);
 }
 
+//Part 2 Class Fantasy
+class Character {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+    }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+//the whole party rolls
+robin.roll();
+robin.companion.roll();
+robin.companion.companion.roll();
+
+console.log(`The party's current inventory is Robins's ${robin.inventory} and Frank's ${robin.companion.companion.inventory}`);
+console.log(`The party's current inventory is Robin's: ${robin.inventory.join(", ")} and Frank's: ${robin.companion.companion.inventory.join(", ")}.`);
